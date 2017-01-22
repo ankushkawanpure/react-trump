5b32dfdbbda01021ddc051df9028fb78eedc941c
Stevie Hetelekides <hetelek@gmail.com>
2017-01-21 17:41:35 -0800

    Add Trump Preloader

--- src/components/TrumpContent/index.jsx
+++ src/components/TrumpContent/index.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './trump_content.css'
 
+import TrumpPreloader from '../TrumpPreloader'
+
 class TrumpContent extends Component {
   render() {
     return (
       <div className="TrumpContent">
-        <p>Lets put our main content here.</p>
+        <TrumpPreloader />
       </div>
     );
   }
