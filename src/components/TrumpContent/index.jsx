b2465f82c841215ea6b6015246a2c45b63cd0540
Stevie Hetelekides <hetelek@gmail.com>
2017-01-22 12:49:48 -0800

    Add BaneAudio component

--- src/components/TrumpContent/index.jsx
+++ src/components/TrumpContent/index.jsx
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import './trump_content.css'
 
 import TrumpPreloader from '../TrumpPreloader'
+import BaneAudio from '../BaneAudio'
 
 class TrumpContent extends Component {
   render() {
     return (
       <div className="TrumpContent">
+        <BaneAudio />
         <TrumpPreloader />
       </div>
     );
