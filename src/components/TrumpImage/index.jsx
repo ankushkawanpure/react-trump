2104467ed693e7c5fe6be101ffb06a32ac58fa47
Louis Vichy <lgvichy@gmail.com>
2017-01-22 14:27:57 -0800

    Hair animated upon hovering

--- src/components/TrumpImage/index.jsx
+++ src/components/TrumpImage/index.jsx
@@ -2,25 +2,16 @@ import React, {Component} from 'react';
 
 import './index.css';
 
-import classNames from 'classnames';
-
 export default class ClassName extends Component {
-
-
 
   toggleHair(){
 
   }
 
   render() {
-    let hairClass = classNames({
-      'TrumpHair': true,
-      'TrumpHairAnimated': this.state.isPressed,
-    });
-
     return (
       <div className="TrumpImageContainer">
-        <div className={hairClass} onClick={toggleHair}></div>
+        <div className="TrumpHair"></div>
         <div className="TrumpBody"></div>
         <div className="TrumpEyes"></div>
         <div className="BaneMask"></div>
